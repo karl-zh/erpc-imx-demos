6139215e67c18aa59d1e4b106208e8f87e3cf8e7
Karl Zhang <karl.zhang@linaro.org>
2019-03-08 14:29:11 +0800

    out put liberpc.a with services
    Signed-off-by: Karl Zhang <karl.zhang@linaro.org>

--- MCU/example_erpc/build/armgcc/imx6sx_sdb_m4/build_all.sh
+++ MCU/example_erpc/build/armgcc/imx6sx_sdb_m4/build_all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cmake -DCMAKE_TOOLCHAIN_FILE="../armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug  .
-make -j4
+#cmake -DCMAKE_TOOLCHAIN_FILE="../armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug  .
+#make -j4
+export ARMGCC_DIR=/usr/local/gcc-arm-none-eabi-7-2018-q2-update
 cmake -DCMAKE_TOOLCHAIN_FILE="../armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release  .
 make -j4
